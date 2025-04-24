60dadc0bc289a8c6b5e083083ca3b7fa12b30b8b
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-04-24 23:04:10 +0200

    Sign Up and Sign in page created, and code for the sign page has been writen but havent been tested yet and the build gradle files have been changed and synced with no errors

---
+++ app/src/main/java/vcmsa/projects/buggybank/Sign_in.kt
@@ -0,0 +1,54 @@
+package vcmsa.projects.buggybank
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class Sign_in : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        auth = Firebase.auth
+        setContentView(R.layout.activity_sign_in)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        
+       
+       
+    }
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        updateUI(currentUser)
+//        customToken?.let {
+//            auth.signInWithCustomToken(it)
+//                .addOnCompleteListener(this) { task ->
+//                    if (task.isSuccessful) {
+//                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "signInWithCustomToken:success")
+//                        val user = auth.currentUser
+//                        updateUI(user)
+//                    } else {
+//                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+//                        Toast.makeText(baseContext, "Authentication failed.",
+//                            Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+//                    }
+//                    Firebase.auth.signOut()
+//    }
+//}
+   // }
+}
