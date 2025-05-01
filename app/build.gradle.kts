7a3fc9215cb726fff3814dd9bc8857cfc0516c3f
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-05-01 20:03:52 +0200

    there is no errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,21 +48,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.firebase.database)
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation(platform(libs.firebase.bom))
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    
     implementation(libs.firebase.database)
-    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    implementation(libs.firebase.auth)
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation(libs.play.services.auth)
     implementation(libs.mpandroidchart)
-    
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
